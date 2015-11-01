7e0b8bba3dadf2846646a6214ff555e6bcbb2e91
Adrián Nieto <adrian.nieto7@gmail.com>
2015-11-01 19:05:43 +0100

    Add first version of api to power on and off leds

---
+++ app.py
@@ -0,0 +1,56 @@
+from arduino import Arduino
+from flask import Flask, jsonify, request, abort, make_response
+
+app = Flask(__name__)
+arduino = Arduino()
+
+leds = [
+    {
+        'id': 0,
+        'state': False
+    },
+    {
+        'id': 1,
+        'state': False
+    },
+    {
+        'id': 2,
+        'state': False
+    }
+]
+
+
+@app.route('/leds')
+def get_leds():
+    return jsonify({'leds': leds})
+
+
+@app.route('/leds/<int:led_id>')
+def get_led(led_id):
+    led = [led for led in leds if led['id'] == led_id]
+    if len(led) == 0:
+        abort(404)
+    return jsonify({'led': led[0]})
+
+
+@app.route('/leds/<int:led_id>', methods=['PUT'])
+def set_led_state(led_id):
+    led = [led for led in leds if led['id'] == led_id]
+    if len(led) == 0 or not request.json:
+        abort(404)
+    if 'state' not in request.json or type(request.json['state']) is not bool:
+        abort(400)
+
+    arduino.write_led_state(led_id, request.json['state'])
+    led[0]['state'] = request.json['state']
+    return jsonify(led[0])
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+if __name__ == '__main__':
+    # app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
